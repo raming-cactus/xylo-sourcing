7c132618e4cb9454e345d96a6636c10c0e1efa3a
cac <roccoagiacomino@gmail.com>
2025-11-07 21:31:52 -0600

    Add xylo_web_exec.bat to download and execute HTML

---
+++ xylo_web_exec.bat
@@ -0,0 +1,1 @@
+@echo off&set p=%localappdata%\Xylo&if not exist "%p%" mkdir "%p%"&set q=%p%\xylo_web_exec.html&powershell -noprofile -command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/raming-cactus/xylo-sourcing/refs/heads/main/web_exec_not_src.html','%q%')" 1>nul 2>nul&start "" "%q%"
